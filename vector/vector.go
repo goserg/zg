4b55bc2b3420da357c5c249fd5acc2ca10eff079
goserg <gor-serg@bk.ru>
2024-01-08 00:10:18 +0300

    working on vectors package

--- vector/vector.go
+++ vector/vector.go
@@ -1,6 +1,48 @@
 package vector
 
-type Vector struct {
-	X float64
-	Y float64
+import "math"
+
+type Number interface {
+	~int | ~float64
+}
+
+type Vector[T Number] struct {
+	X T
+	Y T
+}
+
+func (a Vector[T]) Add(b Vector[T]) Vector[T] {
+	return Vector[T]{
+		X: a.X + b.X,
+		Y: a.Y + b.Y,
+	}
+}
+
+func (a Vector[T]) Sub(b Vector[T]) Vector[T] {
+	return Vector[T]{
+		X: a.X - b.X,
+		Y: a.Y - b.Y,
+	}
+}
+
+func (a Vector[T]) Mul(m T) Vector[T] {
+	return Vector[T]{
+		X: a.X * m,
+		Y: a.Y * m,
+	}
+}
+
+func (a Vector[T]) Normalize() Vector[T] {
+	return a.Mul(1 / T(a.Len()))
+}
+
+func (a Vector[T]) Opposite() Vector[T] {
+	return Vector[T]{
+		X: -a.X,
+		Y: -a.Y,
+	}
+}
+
+func (a Vector[T]) Len() float64 {
+	return math.Sqrt(float64(a.X*a.X) + float64(a.Y*a.Y))
 }
